8b80161f917d457eb545c4401c00b4980206887f
Slo <sloversion2@gmail.com>
2020-07-31 22:51:03 +0700

    add login function

--- src/main/java/com/group5/mycomics/config/WebSecurityConfig.java
+++ src/main/java/com/group5/mycomics/config/WebSecurityConfig.java
@@ -1,13 +1,30 @@
 package com.group5.mycomics.config;
 
+import com.group5.mycomics.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Autowired
+    public void globalConfigure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -15,5 +32,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/user").access("hasAnyRole('ROLE_USER','ROLE_ADMIN')");
         http.authorizeRequests().antMatchers("/admin").access("hasAnyRole('ROLE_ADMIN')");
         http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/admin/403.html");
+        http.authorizeRequests().and().formLogin().loginPage("/login.html").loginProcessingUrl("/j_spring_security_check").defaultSuccessUrl("/index.html").passwordParameter("password").usernameParameter("email");
     }
 }
